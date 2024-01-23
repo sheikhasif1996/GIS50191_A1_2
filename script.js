cc7d6e9a02700c898d870fabc1e6bae70ba787bc
Sk Muhammad Asif <91278606+sheikhasif1996@users.noreply.github.com>
2024-01-22 19:09:35 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+var map = L.map('map').setView([41.8781, -87.6298], 10);
+
+// Load a tile layer
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  subdomains: 'abcd',
+  minZoom: 0,
+  maxZoom: 20,
+  ext: 'png'
+}).addTo(map);
+
+// Load GeoJSON from an external file
+$.getJSON("https://raw.githubusercontent.com/orhuna/WebGIS_SLU_M1/main/Module%201/Assignment%201/data/sf_crime.geojson", function (data) {
+  // Add GeoJSON layer to the map once the file is loaded
+  L.geoJson(data, {
+    pointToLayer: function (feature, latlng) {
+      // Use ratIcon for styling markers
+      return L.marker(latlng, { icon: ratIcon });
+    }
+  }).addTo(map);
+});
+
+var ratIcon = L.icon({
+  iconUrl: 'http://maptimeboston.github.io/leaflet-intro/rat.gif',
+  iconSize: [50, 40]
+});
